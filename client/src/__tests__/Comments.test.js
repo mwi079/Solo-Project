328a4441d6067281d8836648a81e75dadf79a79d
DanB-Web <dan.bayford@gmail.com>
2021-01-22 10:51:15 +0000

    Testing new FE branch

--- client/src/__tests__/Comments.test.js
+++ client/src/__tests__/Comments.test.js
@@ -3,11 +3,29 @@ import ReactDOM from 'react-dom';
 
 import Comments from '../Components/Comments/Comments';
 
-//Comments Component
-test('COMMENTS: Renders correct default text when no comments supplied', () => {
-  const container = document.createElement('div');
-  const topic = {comments: []}
-  ReactDOM.render(<Comments topic= {topic}/>, container);
-  expect(container.textContent).toMatch('No comments yet.. 😕');
-})
+//Alternative syntax for individual tests
+// test('Renders correct default text when no comments supplied', () => {
+//     const container = document.createElement('div');
+//     const topic = {comments: []}
+//     ReactDOM.render(<Comments topic= {topic}/>, container);
+//     expect(container.textContent).toMatch('No comments yet.. 😕');
+//   }
+// );
+
+describe("Comments Component", ()=> {
+  it('Renders correct default text when no comments supplied', () => {
+    const container = document.createElement('div');
+    const topic = {comments: []}
+    ReactDOM.render(<Comments topic= {topic}/>, container);
+    expect(container.textContent).toMatch('No comments yet.. 😕');
+  }),
+
+  it('Renders a comment correctly', () => {
+    const container = document.createElement('div');
+    const topic = {comments: ['Comment 1', 'Comment 2','Comment 3']}
+    ReactDOM.render(<Comments topic= {topic}/>, container);
+    console.log(container);
+  })
+});
+
 
