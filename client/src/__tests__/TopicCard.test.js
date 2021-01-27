87eec3e52fb964acbb6083525659f782b34c0cb1
mwi079 <malcolm.wilkie29@gmail.com>
2021-01-27 09:56:53 +0000

    add topic to typescript

--- client/src/__tests__/TopicCard.test.js
+++ client/src/__tests__/TopicCard.test.js
@@ -1,38 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import { getByTestId } from '@testing-library/dom';
+import { getByTestId } from "@testing-library/react";
 
-import { StateContext } from '../global.context/globalStore.reducer';
+import { StateContext } from "../global.context/globalStore.reducer";
 
-import { dummyTopic } from './DummyData';
+import { dummyTopic } from "./DummyData";
 
-import TopicCard from '../Components/TopicCard/TopicCard';
+import TopicCard from "../Components/TopicCard/TopicCard";
 
 //Context provider is expecting a value of state.isAuth and a function for dispatch
 const dispatch = () => {};
-const state = {isAuth: true };
+const state = { isAuth: true };
 
 //Topic Card Component
-describe('Topics Component', () => {
-
-  it('Renders a topic on the supplied info', () => {
-
-    const container = document.createElement('div');
+describe("Topics Component", () => {
+  it("Renders a topic on the supplied info", () => {
+    const container = document.createElement("div");
     const topic = dummyTopic;
-    
+
     //Render component to container div
     ReactDOM.render(
-    <StateContext.Provider value={{state, dispatch}}>
-      <TopicCard topic= {topic}/>
-    </StateContext.Provider>, container);
-  
+      <StateContext.Provider value={{ state, dispatch }}>
+        <TopicCard topic={topic} />
+      </StateContext.Provider>,
+      container
+    );
+
     //Perform required tests
     const heading = getByTestId(container, "topic-title");
-    expect(heading.textContent).toEqual(topic.title);   
+    expect(heading.textContent).toEqual(topic.title);
     const authorName = getByTestId(container, "topic-author");
     expect(authorName.textContent).toEqual(topic.author.name);
-    
-  })
-
-})
+  });
+});
--- client/src/__tests__/TopicGrid.test.js
+++ client/src/__tests__/TopicGrid.test.js
@@ -1,41 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import { StateContext } from '../global.context/globalStore.reducer';
+import { StateContext } from "../global.context/globalStore.reducer";
 
-import { dummyTopic } from './DummyData';
+import { dummyTopic } from "./DummyData";
 
-import TopicsGrid from '../Components/TopicsGrid/TopicsGrid';
+import TopicsGrid from "../Components/TopicsGrid/TopicsGrid";
 
 //Context provider is expecting a value of state.isAuth and a function for dispatch
 const dispatch = () => {};
-const state = {isAuth: true }
+const state = { isAuth: true };
 
 //Dummy fn + topics []
 const dummyFn = () => {};
 const topics = [];
 
-
-describe('Topics Grid', () => {
-  it('Renders the correct number of topics', () => {
-    const container = document.createElement('div');
+describe("Topics Grid", () => {
+  // eslint-disable-next-line no-unused-expressions
+  it("Renders the correct number of topics", () => {
+    const container = document.createElement("div");
     topics.push(dummyTopic);
     topics.push(dummyTopic);
     topics.push(dummyTopic);
-    ReactDOM.render(<StateContext.Provider value={{state, dispatch}}>
-      <TopicsGrid topics = {topics} setTopics = {dummyFn}/>
-    </StateContext.Provider>, container);
-    const rendered = container.querySelectorAll('.topic-card');
+    ReactDOM.render(
+      <StateContext.Provider value={{ state, dispatch }}>
+        <TopicsGrid topics={topics} setTopics={dummyFn} />
+      </StateContext.Provider>,
+      container
+    );
+    const rendered = container.querySelectorAll(".topic-card");
     expect(rendered.length).toEqual(3);
   }),
-
-  it('Renders correct default text when no topics supplied', () => {
-    const container = document.createElement('div');
-    const topics = null;
-    ReactDOM.render(<StateContext.Provider value={{state, dispatch}}>
-      <TopicsGrid topics = {topics} setTopics = {dummyFn}/>
-    </StateContext.Provider>, container);
-     expect(container.textContent).toMatch('Sorry, there are no quesitons yet. Hurry, and be the first one to begin the conversation! 🗣️');
-  })
-}
-)
+    it("Renders correct default text when no topics supplied", () => {
+      const container = document.createElement("div");
+      const topics = null;
+      ReactDOM.render(
+        <StateContext.Provider value={{ state, dispatch }}>
+          <TopicsGrid topics={topics} setTopics={dummyFn} />
+        </StateContext.Provider>,
+        container
+      );
+      expect(container.textContent).toMatch(
+        "Sorry, there are no quesitons yet. Hurry, and be the first one to begin the conversation! 🗣️"
+      );
+    });
+});
